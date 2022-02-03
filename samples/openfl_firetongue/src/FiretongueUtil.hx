ceb40cd05fdaeedebe248494d5f8dc9c5cbf7922
DotWithATopHat <52358442+DotWith@users.noreply.github.com>
2022-02-02 20:51:23 -0500

    Fixed the openfl parameter

--- samples/openfl_firetongue/src/FiretongueUtil.hx
+++ samples/openfl_firetongue/src/FiretongueUtil.hx
@@ -4,7 +4,7 @@ import firetongue.FireTongue;
 
 class FiretongueUtil
 {
-	public static var firetongue:FireTongue = new FireTongue(Framework.OpenFL);
+	public static var firetongue:FireTongue = new FireTongue(Framework.OPENFL);
 
   public static function t(key:String, ?namespace:String = "data"):String {
     return firetongue.get(key, namespace, true);
